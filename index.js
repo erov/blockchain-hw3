caff46e8dd8bab7d618badab41f6151cb94e15de
Egor Erov <erove@inbox.ru>
2022-11-18 14:48:32 +0300

    + docs

--- index.js
+++ index.js
@@ -7,21 +7,26 @@ const { ethers } = require("ethers");
 main();
 
 async function main() {
+
+  // Checks if token id present.
   if (process.argv.length != 3) {
     console.log("USAGE: node index.js TOKEN")
     exit(1)
   }
-
   const token = process.argv[2]
 
+  // Connects to Alchemy.
   const settings = {
     apiKey: token,
     network: Network.ETH_MAINNET
   };
   const alchemy = new Alchemy(settings)
   const web3 = new Web3("https://eth-mainnet.g.alchemy.com/v2/" + token)
+
+  // Parse oracles contracts abi for work with them via API.
   const abi = JSON.parse(fs.readFileSync('config/abi.json', 'utf-8'))
 
+  // Oracle addresses.
   listeningContracts = new Map([
     ["ETH/USD ", "0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419"],
     ["LINK/ETH", "0xdc530d9457755926550b59e8eccdae7624181557"],
@@ -29,12 +34,17 @@ async function main() {
     ["BTC/USD ", "0xf4030086522a5beea4988f8ca5b36dbc97bee88c"]
   ])
 
+  // Helpers:
+
+  // Converts UNIX timestamp in s into Date.
   timestamp2Date        = timestamp => new Date(timestamp * 1000)
 
+  // Converts token into its presentation without decimals.
   convert2OriginalUnit  = function (price, decimals) {
     return price / (10 ** decimals)
   }
 
+  // Prints log into console.
   printLog              = function(time, type, tokensPairName, price) {
     console.log("%s    [%s]    %s    %f",
                 time,
@@ -45,18 +55,21 @@ async function main() {
   }
 
   listeningContracts.forEach(async function(oracleFeed, tokensPairName) {
+    // Getting contract & aggregator info.
     const contract = await new web3.eth.Contract(abi, oracleFeed)
     const answer = await contract.methods.latestAnswer().call()
     const time = await contract.methods.latestTimestamp().call()
     const decimals = await contract.methods.decimals().call()
     const aggregator = await contract.methods.aggregator().call()
 
+    // Logs latest block info.
     printLog(timestamp2Date(time),
               "latestAnswer",
               tokensPairName,
               convert2OriginalUnit(answer, decimals)
     )
 
+    // Subsribes for latest block updates.
     alchemy.ws.on(
       {
         address: aggregator,
@@ -73,5 +86,4 @@ async function main() {
     );
   });
 }
-
 
